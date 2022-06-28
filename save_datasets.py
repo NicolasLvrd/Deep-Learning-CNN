947c0d7343ead27948646f1709a9affb9f835776
Nicolas Levrard <56602951+NicolasLvrd@users.noreply.github.com>
2022-06-28 17:32:07 +0200

    Delete save_datasets.py

--- save_datasets.py
+++
@@ -1,9 +0,0 @@
-import torch
-from datasets import *
-
-for fold in range(4):
-    train_dataset = TrainDataset(fold)
-    torch.save(train_dataset, "./data/train/train_fold"+str(fold)+".pt")
-
-    test_dataset = TestDataset(fold)
-    torch.save(test_dataset, "./data/test/test_fold"+str(fold)+".pt")
